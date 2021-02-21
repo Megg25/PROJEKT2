9dc9dcd1ce8e4c52c7dfbfce007f6ef67ed4d8b7
Megg25 <75179232+Megg25@users.noreply.github.com>
2021-02-21 13:29:11 +0100

    PROJEKT2 oprava

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-
 def tajne_cislo():
     num = []
     pocet_cisel = 4
@@ -12,25 +11,43 @@ def tajne_cislo():
                 i += 1
     return num
 
+def tvoje_cislo_spravne(tvoje_cislo):
+    list_tvoje_cislo = list(tvoje_cislo)
+    kontrola_spravnosti = True
+    if list_tvoje_cislo[0] == str(0):
+        kontrola_spravnosti = False
+        print("Sorry, the number entered must not begin with ´0´.")
+    if len(list_tvoje_cislo) >= 5 or len(list_tvoje_cislo) <= 3:
+        print("Sorry, the number you entered does not have the required length")
+        kontrola_spravnosti = False
+        exit()
+    for n in list_tvoje_cislo:
+        if n.isalpha():
+            print("Sorry, the number entered cannot contain letters")
+            kontrola_spravnosti = False
+    return kontrola_spravnosti
+
 def spravna_pozice(tvoje_cislo, vybrane_cislo):
     list_tvoje_cislo = list(tvoje_cislo)
     bulls = 0
+    cows = 0
     if list_tvoje_cislo[0] == str(vybrane_cislo[0]):
         bulls += 1
+    elif list_tvoje_cislo[0] in str(vybrane_cislo):
+        cows += 1
     if list_tvoje_cislo[1] == str(vybrane_cislo[1]):
         bulls += 1
+    elif list_tvoje_cislo[1] in str(vybrane_cislo):
+        cows += 1
     if list_tvoje_cislo[2] == str(vybrane_cislo[2]):
         bulls += 1
+    elif list_tvoje_cislo[2] in str(vybrane_cislo):
+        cows += 1
     if list_tvoje_cislo[3] == str(vybrane_cislo[3]):
         bulls += 1
-    return bulls
-
-def spravne_cislo(tvoje_cislo, vybrane_cislo):
-    cows = 0
-    for i in list(tvoje_cislo):
-        if i in str(vybrane_cislo):
-            cows += 1
-    return cows
+    elif list_tvoje_cislo[3] in str(vybrane_cislo):
+        cows += 1
+    return bulls, cows
 
 def vyhodnotit(bulls):
     hodnoceni = 0
@@ -59,13 +76,15 @@ def statistika(soucet_pokusu):
 def hlavni():
     print("Hello, I've generated a random 4 digit number for you.\nLet s play a BULLS and COWS game.")
     vybrane_cislo = tajne_cislo()
-    print(vybrane_cislo)
+    print(vybrane_cislo)  # iba na overenie- ci pocitame spravne!!! potom odstranit!!!
     hodnoceni = 0
     soucet_pokusu = 0
     while hodnoceni != 4:
         tvoje_cislo = input("Enter your 4 digit number: ")
-        bulls = spravna_pozice(tvoje_cislo, vybrane_cislo)
-        cows = spravne_cislo(tvoje_cislo, vybrane_cislo)
+        kontrola_spravnosti = tvoje_cislo_spravne(tvoje_cislo)
+        if kontrola_spravnosti == False:
+            continue
+        bulls, cows = spravna_pozice(tvoje_cislo, vybrane_cislo)
         print(f"{bulls} bulls, {cows} cows")
         hodnoceni = vyhodnotit(bulls)
         soucet_pokusu += 1
@@ -74,4 +93,5 @@ def hlavni():
             print(f"Congratulation! You´ve guessed the right number in {soucet_pokusu} guesses.")
             print(f"That´s{vysledek}")
             exit()
-hlavni()
+
+print(hlavni())
